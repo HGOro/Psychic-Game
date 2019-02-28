5f1282a86ab6cf411144915788a99225e1f5aa75
HG_ORO <h.g.orozco@gmail.com>
2019-02-27 23:54:54 -0800

    Revised functions and html/js consistency

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,42 +7,43 @@ var wins = 0;
 //Losses Counter
 var losses = 0;
 //Guesses Left Counter
-var guesses = 10
+var guessesLeft = 10
 
 //Your Guesses So Far display
+var wrongGuesses = ""
 
 //Computer Random Letter Generator (Works!)
 var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
 console.log(computerChoice)
 
 //PLAYER INPUT
-//When the player presses a KEY it will either +1 to wins counter, or -1 from guesses
+//When the player presses a KEY it will either +1 to wins counter, or -1 from guessesLeft
 //Need an onkey function
 document.onkeypress = function(event){
     var playerGuess = event.key;
+    //do I need that lowerCase converter?
+    var letter= event.key.toLowerCase();
 
     if (playerGuess === computerChoice){
         wins++;
     }
-//if playerGuess does not = computer choice, -1 from guesses
+//if playerGuess does not = computer choice, -1 from guessesLeft
     else {
-        guesses--;
+        guessesLeft--;
     }
 
 //If guesses reaches 0, then +1 to losses
-    if (guesses === 0){
+    if (guessesLeft === 0){
         losses++
     }
 
+//if playerGuess does not = computerChoice, keyevent logged to wrongGuesses
 
-//if playerGuess does not = computerChoice, keyevent logged to Your Guesses So Far
 
-//do I need that lowerCase converter?
-    var letter= event.key.toLowerCase();
 
 document.getElementById('wins').innerHTML = "Wins: " + wins;
 document.getElementById("losses").innerHTML ="Losses: " + losses;
-document.getElementById("guesses").innerHTML ="Guesses Left: " + guesses;
+document.getElementById("guessesLeft").innerHTML ="Guesses Left: " + guessesLeft;
 
 }
 
